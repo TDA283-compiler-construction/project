79ce593fb74928cc9bdfa560b80fdb7e962e3601
Oskar Abrahamsson <oskar8192@gmail.com>
2019-03-26 14:41:00 +0100

    Add leading underscores to names if -fmacho64

--- resources/runtime.s
+++ resources/runtime.s
@@ -1,8 +1,19 @@
 [BITS 64]
 
-extern _printf
-extern _puts
-extern _scanf
+%ifidn __OUTPUT_FORMAT__, macho64
+  %define label(X) _ %+ X
+%elifidn __OUTPUT_FORMAT__, elf64
+  %define label(X) X
+%else
+  %error "Format needs to be macho64 or elf64."
+%endif
+
+%define call_(X) call label(X)
+%define jmp_(X)  jmp  label(X)
+
+extern label(printf)
+extern label(puts)
+extern label(scanf)
 
 section .data
 
@@ -13,40 +24,40 @@ ffmt2   db      "%lf",  0x0
 
 section .text
 
-global _printString
-_printString:
-        jmp     _puts
+global label(printString)
+label(printString):
+        jmp_    (puts)
 
-global _printInt
-_printInt:
+global label(printInt)
+label(printInt):
         mov     rsi, rdi
         lea     rdi, [rel ifmt1]
-        jmp     _printf
+        jmp_    (printf)
 
-global _printDouble
-_printDouble:
+global label(printDouble)
+label(printDouble):
         lea     rdi, [rel ffmt1]
         mov     al, 1
-        jmp     _printf
+        jmp_    (printf)
 
-global _readInt
-_readInt:
+global label(readInt)
+label(readInt):
         push    rax
         lea     rdi, [rel ifmt2]
         lea     rsi, [rsp + 4]
         xor     eax, eax
-        call    _scanf
+        call_   (scanf)
         mov     eax, [rsp + 4]
         pop     rcx
         ret
 
-global _readDouble
-_readDouble:
+global label(readDouble)
+label(readDouble):
         push    rax
         lea     rdi, [rel ffmt2]
         mov     rsi, rsp
         xor     eax, eax
-        call    _scanf
+        call_   (scanf)
         movsd   xmm0, [rsp]
         pop     rax
         ret
