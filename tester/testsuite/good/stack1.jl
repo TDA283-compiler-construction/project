2b8b071682c636df9bad5b8f3318c433b89d74e5
Oskar Abrahamsson <oskarabrahamsson@users.noreply.github.com>
2021-06-10 15:24:29 +0200

    Add two tests to check variable initialization
    These tests attempt to pollute the stack frame with (recognizable) junk,
    so that we can see whether student compilers initialize variables to
    their defaults. Appears to work under the Docker image.

---
+++ tester/testsuite/good/stack1.jl
@@ -0,0 +1,44 @@
+void foo()
+{
+    int x01 = 1;
+    int x02 = 2;
+    int x03 = 3;
+    int x04 = 4;
+    int x05 = 5;
+    int x06 = 6;
+    int x07 = 7;
+    int x08 = 8;
+    int x09 = 9;
+    int x10 = 10;
+}
+
+void bar()
+{
+    int x01;
+    int x02;
+    int x03;
+    int x04;
+    int x05;
+    int x06;
+    int x07;
+    int x08;
+    int x09;
+    int x10;
+    printInt(x01);
+    printInt(x02);
+    printInt(x03);
+    printInt(x04);
+    printInt(x05);
+    printInt(x06);
+    printInt(x07);
+    printInt(x08);
+    printInt(x09);
+    printInt(x10);
+}
+
+int main()
+{
+    foo();
+    bar();
+    return 0;
+}
