1ae80c410a214ad414d7ef356ca5bca503e2ba91
gusbacke <gusbacke@users.noreply.github.com>
2021-07-10 15:13:22 +0200

    Testing increment/decrement for doubles, which is not allowed.

---
+++ tester/testsuite/bad/bad067.jl
@@ -0,0 +1,10 @@
+// bad067.jl
+
+    /* Test increment/decrements for doubles. Only int allowed */
+    int main () {
+      double x = 1.0 ;
+      x++;  // Only for variables of type int
+      x--;  // Only for variables of type int
+      return 0;
+    }
+
