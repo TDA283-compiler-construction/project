9e4eb2cacd6c29e85c3aa78553790f8f8332c0e2
Burak Bilge Yalcinkaya <bbyalcinkaya@gmail.com>
2022-03-15 11:05:37 +0100

    update good/order_binop.jl

--- tester/testsuite/good/order_binop.jl
+++ tester/testsuite/good/order_binop.jl
@@ -1,10 +1,20 @@
 
-int main(){
-    printInt(foo(12) + foo(34));
+int main() {
+
+    int x;
+    
+    x = f(1) - f(2);
+    x = f(3) + f(4);
+    x = f(5) * f(6);
+    x = f(7) / f(8);
+    x = f(9) % f(2);
+ 
+    printInt(f(12) + f(34));
+
     return 0;
 }
 
-int foo(int x) {
+int f(int x) {
     printInt(x);
     return x;
 }
