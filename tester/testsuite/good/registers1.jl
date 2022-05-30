725d4d9eaff2edb1f97bc329d63770fd1b00f78a
Frans Bergman <fransb@student.chalmers.se>
2022-05-30 22:58:04 +0200

    Add tests for register exhaustion
    Functions with many local variables or many arguments may exhaust
    available CPU registers when performing register allocation in native
    code generation.

---
+++ tester/testsuite/good/registers1.jl
@@ -0,0 +1,38 @@
+int main()
+{
+    int x01 = 1;
+    int x02 = 2;
+    int x03 = 3;
+    int x04 = 4;
+    int x05 = 5;
+    int x06 = 6;
+    int x07 = 7;
+    int x08 = 8;
+    int x09 = 9;
+    int x10 = 10;
+    int x11 = 11;
+    int x12 = 12;
+    int x13 = 13;
+    int x14 = 14;
+    int x15 = 15;
+    int x16 = x01 + x02 + x03 + x04 + x05 + x06 + x07 + x08 +
+              x09 + x10 + x11 + x12 + x13 + x14 + x15;
+
+    printInt(x01);
+    printInt(x02);
+    printInt(x03);
+    printInt(x04);
+    printInt(x05);
+    printInt(x06);
+    printInt(x07);
+    printInt(x08);
+    printInt(x09);
+    printInt(x10);
+    printInt(x11);
+    printInt(x12);
+    printInt(x13);
+    printInt(x14);
+    printInt(x15);
+    printInt(x16);
+    return 0;
+}
