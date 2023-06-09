6c154c8f7dbae0ece9f8f1762d299fa1a3d62d8f
oskeri <oskeri@chalmers.se>
2023-06-09 14:54:29 +0200

    Mixed arrays1 and objects1 tests

---
+++ tester/testsuite/extensions/arrays1_objects1/mixed002.jl
@@ -0,0 +1,47 @@
+class A {
+    int[] xs;
+
+    void set(int[] as) {
+        xs = as;
+    }
+
+    int[] get() {
+        return xs;
+    }
+
+    int getIndex(int n) {
+        return xs[n];
+    }
+
+}
+
+A init(int n) {
+    A a = new A;
+
+    int[] xs = new int[n];
+    int i = 0;
+    while(i < xs.length) {
+        xs[i] = i;
+        i++;
+    }
+
+    a.set(xs);
+
+    return a;
+}
+
+int main() {
+
+    A a = init(4);
+
+    for(int x : a.get())
+        printInt(x);
+
+    int i = 0;
+    while(i < a.get().length) {
+        printInt(getIndex(i));
+        i++;
+    }
+
+    return 0;
+}
